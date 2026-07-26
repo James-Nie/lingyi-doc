d6f79e12395905f107e91e58eb159e03d236b2f1
niejianbo <jianbo.niejb@dtzhejiang.com>
2026-07-26 09:55:01 +0800

    remove temp folder from tracking

--- packages/lingyi-doc-admin/src/pages/org/OrgMembersPage.tsx
+++
@@ -1,1 +0,0 @@
-export { OrgManagementPage, OrgMembersPage } from './OrgManagementPage';
--- packages/lingyi-doc-admin/src/types/lingyi-doc-shim.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@lingyi-doc/core';
-declare module '@lingyi-doc/editor-doc';
-declare module '@lingyi-doc/editor-mindmap';
-declare module '@lingyi-doc/editor-sheet';
-declare module '@lingyi-doc/editor-whiteboard';
